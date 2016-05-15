b8b0ab8a7f404d25d399196ca817b477ee6e86d8
kepthx <kepthx@gmail.com>
2016-05-15 09:59:57 +0300

    first @angular interpolation

--- app/app.comonent.ts
+++ app/app.comonent.ts
@@ -2,7 +2,13 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "my-app",
-    template: `<h1>My First Angular 2 App</h1>`
+    template: `
+        <h1>{{title}}</h1>
+        <h2>My favorite hero is: {{myHero}}</h2>
+    `
 })
 
-export class AppComponent {}
+export class AppComponent {
+    title = "Tour of Heroes";
+    myHero = "Windstorm";
+}
