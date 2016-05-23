9902cac434841d0e5b2ae2b2d5f6a84ddefd1e85
kepthx <kepthx@gmail.com>
2016-05-23 13:22:45 +0300

    angular 2 forms

--- app/app.comonent.ts
+++ app/app.comonent.ts
@@ -1,14 +1,10 @@
 import { Component } from "@angular/core";
+import { HeroFormComponent } from "./hero-form.component";
 
 @Component({
     selector: "my-app",
-    template: `
-        <h1>{{title}}</h1>
-        <h2>My favorite hero is: {{myHero}}</h2>
-    `
+    template: `<hero-form></hero-form>`,
+    directives: [HeroFormComponent]
 })
 
-export class AppComponent {
-    title = "Tour of Heroes";
-    myHero = "Windstorm";
-}
+export class AppComponent {}
